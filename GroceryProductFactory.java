2006802d0626952bd7f49e993ad466abf44c95ed
Thashon Srikantharajah <thashon.s@hotmail.com>
2022-10-05 11:35:31 -0400

    first commit

---
+++ GroceryProductFactory.java
@@ -0,0 +1,25 @@
+public class GroceryProductFactory {
+    // select value betweeen 1 and 2
+    // 1 = apples 2= bananas
+    // this class used to make different product type factories
+    // this is abstract factory class  
+    // should create object here and later be defined and set in subclass 
+    // subclass should be able to redefine and instantiate in subclass 
+
+    
+
+
+
+public void FactoryMethod (){
+// get value of number to represent which grocery product 
+// go into class to get price from database related to which product 
+// then output price of apple
+// will be used to get price based on info in data file 
+// should redefine object and instantiate here to get price 
+// 
+
+}
+
+
+}
+
